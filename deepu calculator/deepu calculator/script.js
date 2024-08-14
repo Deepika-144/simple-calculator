c26191a84f33a856b469b763d927be34789707e9
Deepika-144 <deepikagalipelli@gmail.com>
2024-08-14 16:20:41 +0530

    Add files via upload

---
+++ deepu calculator/deepu calculator/script.js
@@ -0,0 +1,31 @@
+function appendValue(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function square() {
+    const display = document.getElementById('display');
+    try {
+        display.value = Math.pow(eval(display.value), 2);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
